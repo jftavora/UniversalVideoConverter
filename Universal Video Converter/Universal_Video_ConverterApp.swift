133c6cb02e80f11962a75fc22bc73c45ffe487e2
jftavora <jftavora@gmail.com>
2026-06-12 19:29:03 -0300

    Initial Commit

---
+++ Universal Video Converter/Universal_Video_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Universal_Video_ConverterApp.swift
+//  Universal Video Converter
+//
+//  Created by João Fernando Távora on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Universal_Video_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
